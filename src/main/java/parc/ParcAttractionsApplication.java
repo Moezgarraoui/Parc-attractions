e8cbf3f1f8830c2bc9ff33e6cc4d64690da25700
Moezgarraoui <garraouimouez@gmail.com>
2023-12-13 18:09:21 +0100

    projet

---
+++ src/main/java/parc/ParcAttractionsApplication.java
@@ -0,0 +1,13 @@
+package parc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcAttractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcAttractionsApplication.class, args);
+	}
+
+}
