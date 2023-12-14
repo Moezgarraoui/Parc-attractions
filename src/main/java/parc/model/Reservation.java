5c678a3bac9767c4bef72ec2d783f2fe245b3874
Ismail Fourati <119300453+smail16@users.noreply.github.com>
2023-12-14 10:49:16 +0100

    Merge branch 'main' of https://github.com/Moezgarraoui/Parc-attractions
    class reservation

--- src/main/java/parc/model/Reservation.java
+++ src/main/java/parc/model/Reservation.java
@@ -2,8 +2,6 @@ package parc.model;
 
 import java.time.LocalDate;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
