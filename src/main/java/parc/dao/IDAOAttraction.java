426783245544d26f45937315a569bb0d1ed6f7d9
chloe442544 <chloegravouil@hotmail.fr>
2023-12-14 18:05:40 +0100

    Ajout des fonctions findAllMAdulte et findAllMEnfant

--- src/main/java/parc/dao/IDAOAttraction.java
+++ src/main/java/parc/dao/IDAOAttraction.java
@@ -1,11 +1,20 @@
 package parc.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import parc.model.Attraction;
+import parc.model.MAdulte;
+import parc.model.MEnfant;
 
 
 
 public interface IDAOAttraction extends JpaRepository <Attraction,Integer> {
-	
+	@Query("from MAdulte")
+	public List<MAdulte> findAllMAdulte();
+	@Query("from MEnfant")
+	public List<MEnfant> findAllMEnfant();
 }
