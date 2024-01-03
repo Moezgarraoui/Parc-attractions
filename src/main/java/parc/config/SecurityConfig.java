e142ade83b2ac312d7c627325e9882aac8af2f1c
Moezgarraoui <garraouimouez@gmail.com>
2024-01-03 17:16:18 +0100

    adminRun // préparation SecurityConfig

---
+++ src/main/java/parc/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package parc.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Ajoutez d'autres configurations de sécurité au besoin
+}
